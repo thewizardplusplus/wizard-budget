f16d401600da7e63bece8f446fca788dc5c17664
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-01-22 12:22:03 +0300

    Fixes #17: remove selectize.js library; correct some lint warnings.

--- src/ru/thewizardplusplus/wizardbudget/Widget.java
+++ src/ru/thewizardplusplus/wizardbudget/Widget.java
@@ -6,7 +6,6 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.view.View;
 import android.widget.RemoteViews;
 
 public class Widget extends AppWidgetProvider {
