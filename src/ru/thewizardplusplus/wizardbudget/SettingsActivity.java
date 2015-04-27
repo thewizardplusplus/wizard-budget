55b506a10ce856891d89f7426986577411ab2cde
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-04-27 12:54:16 +0300

    Fixes #38: add preference click handlers.

--- src/ru/thewizardplusplus/wizardbudget/SettingsActivity.java
+++ src/ru/thewizardplusplus/wizardbudget/SettingsActivity.java
@@ -5,6 +5,7 @@ import android.os.*;
 import android.app.*;
 
 import java.util.regex.*;
+import android.view.View.*;
 
 public class SettingsActivity extends PreferenceActivity {
 	@Override
@@ -13,27 +14,25 @@ public class SettingsActivity extends PreferenceActivity {
 		super.onCreate(saved_instance_state);
 		addPreferencesFromResource(R.xml.preferences);
 
-		addValidator(
+		addRegexpValidator(
 			"preference_sms_number_pattern",
 			"Invalid syntax of SMS number pattern."
 		);
-		addValidator(
+		addRegexpValidator(
 			"preference_sms_spending_pattern",
 			"Invalid syntax of SMS spending pattern."
 		);
-		addValidator(
+		addRegexpValidator(
 			"preference_sms_income_pattern",
 			"Invalid syntax of SMS income pattern."
 		);
 	}
 
 	@SuppressWarnings("deprecation")
-	private void addValidator(String preference_name, String error_message) {
+	private void addRegexpValidator(String preference_name, String error_message) {
 		final String error_message_copy = error_message;
-		EditTextPreference sms_number_pattern =
-			(EditTextPreference)getPreferenceScreen()
-			.findPreference(preference_name);
-		sms_number_pattern.setOnPreferenceChangeListener(
+		addValidator(
+			preference_name,
 			new Preference.OnPreferenceChangeListener() {
 				@Override
 				public boolean onPreferenceChange(
@@ -59,6 +58,37 @@ public class SettingsActivity extends PreferenceActivity {
 				}
 			}
 		);
+	}
+
+	@SuppressWarnings("deprecation")
+	private void addValidator(String preference_name, Preference.OnPreferenceChangeListener validator) {
+		EditTextPreference preference =
+			(EditTextPreference)getPreferenceScreen()
+			.findPreference(preference_name);
+		preference.setOnPreferenceChangeListener(validator);
+	}
+
+	@SuppressWarnings("deprecation")
+	private void addChangeWarning(String preference_name, String warning_message) {
+		final String warning_message_copy = warning_message;
+		addClickHandler(
+			preference_name,
+			new Preference.OnPreferenceClickListener() {
+				@Override
+				public boolean onPreferenceClick(Preference preference) {
+					showAlert("Warning!", warning_message_copy);
+					return true;
+				}
+			}
+		);
+	}
+
+	@SuppressWarnings("deprecation")
+	private void addClickHandler(String preference_name, Preference.OnPreferenceClickListener handler) {
+		EditTextPreference preference =
+			(EditTextPreference)getPreferenceScreen()
+			.findPreference(preference_name);
+		preference.setOnPreferenceClickListener(handler);
 	}
 
 	private void showAlert(String title, String message) {
