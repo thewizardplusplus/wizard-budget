96d41fb4c4ff5982e2ee049d7f7e0f393d5b5187
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-04-27 14:55:59 +0300

    Fixes #38: add resetting of passwords.

--- src/ru/thewizardplusplus/wizardbudget/SettingsActivity.java
+++ src/ru/thewizardplusplus/wizardbudget/SettingsActivity.java
@@ -6,6 +6,7 @@ import android.app.*;
 
 import java.util.regex.*;
 import android.view.View.*;
+import android.content.*;
 
 public class SettingsActivity extends PreferenceActivity {
 	@Override
@@ -26,13 +27,47 @@ public class SettingsActivity extends PreferenceActivity {
 			"preference_sms_income_pattern",
 			"Invalid syntax of SMS income pattern."
 		);
+
+		final Context context = this;
+		getPreferenceScreen()
+			.findPreference("preference_dropbox_passwords_reset_button")
+			.setOnPreferenceClickListener(
+				new Preference.OnPreferenceClickListener() {
+					@Override
+					public boolean onPreferenceClick(Preference preference) {
+						showAlert(
+							"Warning!",
+							"Are you sure you want to reset passwords of backuping Dropbox app to defaults?",
+							new DialogInterface.OnClickListener() {
+								@Override
+								public void onClick(DialogInterface dialog, int which) {
+									EditTextPreference dropbox_app_key =
+										(EditTextPreference)getPreferenceScreen()
+										.findPreference("preference_dropbox_app_key");
+									dropbox_app_key.setText(context.getString(R.string.preference_dropbox_app_key_default));
+
+									EditTextPreference dropbox_app_secret =
+										(EditTextPreference)getPreferenceScreen()
+										.findPreference("preference_dropbox_app_secret");
+									dropbox_app_secret.setText(context.getString(R.string.preference_dropbox_app_secret_default));
+								}
+							},
+							true
+						);
+
+						return true;
+					}
+				}
+			);
 	}
 
 	@SuppressWarnings("deprecation")
 	private void addRegexpValidator(String preference_name, String error_message) {
 		final String error_message_copy = error_message;
-		addValidator(
-			preference_name,
+		EditTextPreference preference =
+			(EditTextPreference)getPreferenceScreen()
+			.findPreference(preference_name);
+		preference.setOnPreferenceChangeListener(
 			new Preference.OnPreferenceChangeListener() {
 				@Override
 				public boolean onPreferenceChange(
@@ -50,7 +85,9 @@ public class SettingsActivity extends PreferenceActivity {
 					} catch (PatternSyntaxException exception) {
 						showAlert(
 							"Error!",
-							error_message_copy + " " + exception.getMessage()
+							error_message_copy + " " + exception.getMessage(),
+							null,
+							false
 						);
 					}
 
@@ -60,44 +97,22 @@ public class SettingsActivity extends PreferenceActivity {
 		);
 	}
 
-	@SuppressWarnings("deprecation")
-	private void addValidator(String preference_name, Preference.OnPreferenceChangeListener validator) {
-		EditTextPreference preference =
-			(EditTextPreference)getPreferenceScreen()
-			.findPreference(preference_name);
-		preference.setOnPreferenceChangeListener(validator);
-	}
-
-	@SuppressWarnings("deprecation")
-	private void addChangeWarning(String preference_name, String warning_message) {
-		final String warning_message_copy = warning_message;
-		addClickHandler(
-			preference_name,
-			new Preference.OnPreferenceClickListener() {
-				@Override
-				public boolean onPreferenceClick(Preference preference) {
-					showAlert("Warning!", warning_message_copy);
-					return true;
-				}
-			}
-		);
-	}
-
-	@SuppressWarnings("deprecation")
-	private void addClickHandler(String preference_name, Preference.OnPreferenceClickListener handler) {
-		EditTextPreference preference =
-			(EditTextPreference)getPreferenceScreen()
-			.findPreference(preference_name);
-		preference.setOnPreferenceClickListener(handler);
-	}
-
-	private void showAlert(String title, String message) {
-		new
-			AlertDialog.Builder(this)
+	private void showAlert(
+		String title,
+		String message,
+		DialogInterface.OnClickListener handler,
+		boolean show_cancel
+	) {
+		AlertDialog.Builder builder =
+			new AlertDialog
+			.Builder(this)
 			.setIcon(android.R.drawable.ic_dialog_alert)
 			.setTitle(title)
 			.setMessage(message)
-			.setPositiveButton(android.R.string.ok, null)
-			.show();
+			.setPositiveButton(android.R.string.ok, handler);
+		if (show_cancel) {
+			builder.setNegativeButton(android.R.string.cancel, null);
+		}
+		builder.show();
 	}
 }
