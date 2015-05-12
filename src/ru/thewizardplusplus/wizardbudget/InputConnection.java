712bc01507d6ea82ef2bd9e97430f1ebb3fb8fed
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-05-12 21:22:20 +0300

    Fixes #50: correct custom InputConnection.

--- src/ru/thewizardplusplus/wizardbudget/InputConnection.java
+++ src/ru/thewizardplusplus/wizardbudget/InputConnection.java
@@ -3,18 +3,25 @@ package ru.thewizardplusplus.wizardbudget;
 import android.view.inputmethod.*;
 import android.view.*;
 
-public class InputConnection extends BaseInputConnection implements android.view.inputmethod.InputConnection {
+public class InputConnection extends BaseInputConnection {
 	public InputConnection(View target_view, boolean full_editor) {
 		super(target_view, full_editor);
 	}
 
 	@Override
 	public boolean deleteSurroundingText(int before_length, int after_length) {
-		// in latest Android deleteSurroundingText(1, 0) will be called for Backspace
+		/* In Android 4+ Backspace key not call sendKeyEvent(..., KeyEvent.KEYCODE_DEL).
+		 * Instead it call deleteSurroundingText(1, 0).
+		 * Below first called manually for JavaScript "keydown" and "keyup" events.
+		 */
 		if (before_length == 1 && after_length == 0) {
 			KeyEvent backspace_down = new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL);
+			boolean result_down = super.sendKeyEvent(backspace_down);
+
 			KeyEvent backspace_up = new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_DEL);
-			return super.sendKeyEvent(backspace_down) && super.sendKeyEvent(backspace_up);
+			boolean result_up = super.sendKeyEvent(backspace_up);
+
+			return result_down && result_up;
 		}
 
 		return super.deleteSurroundingText(before_length, after_length);
