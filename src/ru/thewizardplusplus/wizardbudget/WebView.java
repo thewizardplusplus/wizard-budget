4851c038cf7a1e70e246cb65b8f136a98cbbc996
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-05-12 21:20:01 +0300

    Fixes #50: use custom InputConnection.

--- src/ru/thewizardplusplus/wizardbudget/WebView.java
+++ src/ru/thewizardplusplus/wizardbudget/WebView.java
@@ -2,9 +2,16 @@ package ru.thewizardplusplus.wizardbudget;
 
 import android.content.*;
 import android.util.*;
+import android.view.inputmethod.*;
+import android.text.*;
 
 public class WebView extends android.webkit.WebView {
 	public WebView(Context context, AttributeSet attributes) {
 		super(context, attributes);
+	}
+
+	@Override
+	public android.view.inputmethod.InputConnection onCreateInputConnection(EditorInfo out_attributes) {
+		return new InputConnection(this, false);
 	}
 }
