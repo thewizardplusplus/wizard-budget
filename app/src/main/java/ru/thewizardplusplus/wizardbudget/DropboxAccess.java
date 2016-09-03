6a805a3a9e3d84df6a29d7f31c2e4e6c391aa337
thewizardplusplus <thewizardplusplus@yandex.ru>
2016-09-04 01:16:39 +0300

    Update the Dropbox access key

--- app/src/main/java/ru/thewizardplusplus/wizardbudget/DropboxAccess.java
+++ app/src/main/java/ru/thewizardplusplus/wizardbudget/DropboxAccess.java
@@ -1,7 +1,7 @@
 package ru.thewizardplusplus.wizardbudget;
 
 public interface DropboxAccess {
-	public static final String APP_KEY = "a3fjeiuyp2gcndt";
+	public static final String APP_KEY = "b0vzne23tu7i7rb";
 
 	public String getAppSecret();
 }
