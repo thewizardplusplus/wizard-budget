9bf45166e5611fbf99d7d56480226ae527292bc9
thewizardplusplus <thewizardplusplus@yandex.ru>
2024-10-29 15:19:25 +0500

    Show the buy cost sums in the buy widget

--- app/src/main/java/ru/thewizardplusplus/wizardbudget/BuyWidget.java
+++ app/src/main/java/ru/thewizardplusplus/wizardbudget/BuyWidget.java
@@ -5,6 +5,9 @@ import android.content.*;
 import android.widget.*;
 import android.app.*;
 import android.net.*;
+import android.text.*;
+import android.text.style.*;
+import android.graphics.*;
 
 public class BuyWidget extends AppWidgetProvider {
 	public static RemoteViews getUpdatedViews(Context context) {
@@ -33,6 +36,29 @@ public class BuyWidget extends AppWidgetProvider {
 		views.setPendingIntentTemplate(R.id.buy_list, click_pending_intent);
 		views.setEmptyView(R.id.buy_list, R.id.buy_empty_list_stub);
 
+		BuyManager buy_manager = new BuyManager(context);
+		double total_sum = Double.parseDouble(buy_manager.getAnyCostsSum());
+		double monthly_sum = Double.parseDouble(buy_manager.getMonthlyCostsSum());
+		views.setTextViewText(
+			R.id.simple_monthly_buy_list_sum,
+			formatBuySum("Monthly", monthly_sum, Double.NaN)
+		);
+		views.setTextViewText(
+			R.id.monthly_buy_list_sum,
+			formatBuySum("Monthly", monthly_sum, total_sum)
+		);
+		double single_sum = Double.parseDouble(buy_manager.getSingleCostsSum());
+		views.setTextViewText(
+			R.id.single_buy_list_sum,
+			formatBuySum("Single", single_sum, total_sum)
+		);
+
+		boolean only_monthly = Settings.getCurrent(context).isOnlyMonthly();
+		views.setDisplayedChild(
+			R.id.buy_list_sum_view_flipper,
+			only_monthly ? 0 : 1
+		);
+
 		return views;
 	}
 
@@ -50,5 +76,17 @@ public class BuyWidget extends AppWidgetProvider {
 			widget_ids,
 			R.id.buy_list
 		);
+	}
+
+	private static CharSequence formatBuySum(String kind, double sum, double total_sum) {
+		String text = !Double.isNaN(total_sum)
+			? String.format("%s sum: %.2f/%.2f \u20bd", kind, sum, total_sum)
+			: String.format("%s sum: %.2f \u20bd", kind, sum);
+		int title_end_index = text.indexOf(':');
+
+		SpannableString formatted_text = new SpannableString(text); 
+		formatted_text.setSpan(new StyleSpan(Typeface.BOLD), 0, title_end_index + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+		return formatted_text;
 	}
 }
